af0d2113d2169ad7574521cb4511390349b32c15
szyncel <mtteu1994@gmail.com>
2018-04-30 17:09:34 +0200

    -refactor

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { CategoryService } from '../../category.service';
-import { ProductService } from '../../product.service';
-import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
-import { Product } from '../../models/product';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {CategoryService} from '../../category.service';
+import {ProductService} from '../../product.service';
+import {Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
+import {Product} from '../../models/product';
 
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.css']
 })
-export class ProductFormComponent implements OnInit{
+export class ProductFormComponent implements OnInit {
   categoryList;
-  product:Product;
+  product: Product;
   id;
 
   constructor(
@@ -21,31 +21,32 @@ export class ProductFormComponent implements OnInit{
     private router: Router,
     private route: ActivatedRoute
   ) {
-    this.product=new Product();
+    this.product = new Product();
     this.id = this.route.snapshot.paramMap.get('id');
     if (this.id) this.productService.get(this.id).subscribe(p => this.product = p);
   }
 
   save(product) {
+    console.log('pt', product.value);
     if (product.valid) {
       if (this.id) {
         this.productService.update(this.id, product.value)
-          .subscribe((res) =>this.router.navigate(['admin/products']))
-      }else{
+          .subscribe((res) => this.router.navigate(['admin/products']))
+      } else {
         this.productService.create(product.value)
-        .subscribe((res) => {
-          this.router.navigate(['admin/products']);
-        })
+          .subscribe((res) => {
+            this.router.navigate(['admin/products']);
+          })
       }
     } else {
       console.log('Invalid');
     }
   }
 
-  delete(){
-    if(!confirm('Czy jesteś pewny, że chcesz usunać ten produkt?')) return
+  delete() {
+    if (!confirm('Czy jesteś pewny, że chcesz usunać ten produkt?')) return
     this.productService.delete(this.id)
-    .subscribe((res) => console.log(res))
+      .subscribe((res) => console.log(res))
     this.router.navigate(['admin/products']);
   }
 
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,32 +1,33 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
 
-  constructor(private http: HttpClient) { }
-
-  create(product:any):Observable<any>{
-    return this.http.post('/api/product',product);
+  constructor(private http: HttpClient) {
   }
 
-  getAll():Observable<any>{
+  create(product: any): Observable<any> {
+    return this.http.post('/api/product', product);
+  }
+
+  getAll(): Observable<any> {
     return this.http.get('/api/product')
-    .map(res => res["product"]);
+      .map(res => res["product"]);
   }
 
-  get(productId){
+  get(productId) {
     return this.http.get(`/api/product/${productId}`)
-    .map(res=>res["product"]);
+      .map(res => res["product"]);
   }
 
-  update(productId,product){
-    return this.http.put(`/api/product/${productId}`,product);
+  update(productId, product) {
+    return this.http.put(`/api/product/${productId}`, product);
   }
 
-  delete(productId){
+  delete(productId) {
     return this.http.delete(`/api/product/${productId}`);
   }
 }
